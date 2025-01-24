87ab2498b036745dcd54008edf5356b390f7c476
omkar-vardhan <153315675+omkar-vardhan@users.noreply.github.com>
2025-01-24 14:55:46 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a;
+	float b;
+	char c;
+	char str[10];
+	
+	printf("Read the values");
+	scanf("%d %c %f",&a,&c,&b);
+	printf("the vauee of A %a\n The value of B=%b\n The value of C=%c\n",a,b,c);
+}
