c103c41c4a66ec38a6036b15c63ac5bf77585af9
omkar-vardhan <153315675+omkar-vardhan@users.noreply.github.com>
2025-01-24 14:52:09 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a[100],n,i;
+	printf("Enter the size of the array:");
+	scanf("%d",&n);
+	printf("Enter the elements in array:");
+	for(i=0;i<n;i++)
+		scanf("%d\n",a[i]);
+		printf("The array values are:");
+		
+	
+for(i=0;i<n;i++)
+printf("%d\n",a[i]);
+
+}
